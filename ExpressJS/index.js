b20a9a396778f17488200062140e65608f226f1a
Nishchit14 <dnishchit@gmail.com>
2018-05-04 16:28:39 +0530

    Code refactoring

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -1,78 +1,64 @@
 const http = require('http');
-const express = require('express');
 const sockjs = require('sockjs');
-const RoomServer = require('sockjs-rooms').server;
+const express = require('express'),
+    app = express();
 
-const sockjs_opts = {
-    sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js"
-};
-const service = sockjs.createServer(sockjs_opts);
-const server = new RoomServer(service);
+// Clients list
+const clients = {};
 
-/**
- * perticuler channel defined
- */
-let friends = server.registerChannel('friends');
-friends.on('connection', (conn) =>  {
-    conn.write('friends is connected');
-    conn.on('data', (data) => {
-        conn.write(data);
-    });
+// Broadcast to all clients
+const  broadcast = (message) => {
+    // iterate through each client in clients object
+    for (let client in clients){
+        // send the message to that client
+        clients[client].write(JSON.stringify(message));
+    }
+}
+
+// create sockjs server
+const echo = sockjs.createServer();
+
+//let online = []
+// on new connection event
+echo.on('connection', (conn) => {
+
+    online.push(conn.id)
+
+    // add this client to clients object
+    clients[conn.id] = conn;
+    //conn.write(online)
+    const newMessage = (mes) => {
+        console.log(mes);
+        broadcast({message:JSON.parse(mes)})
+    }
+
+    const Disconnect = ()=> {
+        // let index = online.indexOf(conn.id);
+        // if (index > -1) {
+        //     online.splice(index, 1);
+        // }
+        // console.log(`disconnected`,conn.id)
+        delete clients[conn.id];
+    }
+
+    // on receive new data from client event
+    conn.on('data', newMessage)
+
+    // on connection close event
+    conn.on('close',Disconnect)
+
 });
 
-let Family = server.registerChannel('Family');
-Family.on('connection', (conn) => {
-    conn.write('Family is connected');
-    conn.on('data', (data) => {
-        conn.write(data);
-    });
-});
+// Create an http server
+const server = http.createServer(app);
 
-let colleague = server.registerChannel('colleague');
-colleague.on('connection', (conn) => {
-    conn.write('coulegue is connected');
-    conn.on('data', (data) => {
-        conn.write(data);
-        console.log(`sdadsfs`,data)
-    });
-});
+// Integrate SockJS and listen on /echo
+echo.installHandlers(server, {prefix:'/echo'});
 
-let red = server.registerChannel('red');
-red.on('connection', (conn) => {
-    conn.write('Red is connected');
-    conn.on('data', function(data) {
-        conn.write(data);
-    });
-});
+// Start server
+server.listen(9999, '0.0.0.0');
 
-let latency = server.registerChannel('latency');
-latency.on('connection', (conn) => {
-    conn.on('data', (data) => {
-        conn.write(data);
-    });
-});
-
-/**
- * close channel
- */
-red.on('close',() => {
-    console.log("SERVER: red channel was closed!");
-});
-
-red.on('end', () => {
-    console.log("SERVER: red channel was end!");
-});
-
-let app = express();
-let serv = http.createServer(app);
-
-service.installHandlers(serv, {
-    prefix: '/multiplex'
-});
-
-console.log(' [*] Listening on 0.0.0.0:9999');
-serv.listen(9999, '0.0.0.0');
 // Route for chat
-app.get('/group', (req, res)=> {
-    res.sendFile('group.html', {root: './public'});
+app.get('/chat', (req, res)=> {
+    res.sendFile('chat.html', {root: './public'});
 });
--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -1,64 +1,82 @@
 const http = require('http');
+const express = require('express');
 const sockjs = require('sockjs');
-const express = require('express'),
-          app = express();
+const RoomServer = require('sockjs-rooms').server;
 
-// Clients list
-const clients = {};
+const sockjs_opts = {
+    sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.min.js"
+};
+const service = sockjs.createServer(sockjs_opts);
+const server = new RoomServer(service);
 
-// Broadcast to all clients
-const  broadcast = (message) => {
-    // iterate through each client in clients object
-    for (let client in clients){
-        // send the message to that client
-        clients[client].write(JSON.stringify(message));
-    }
-}
-
-// create sockjs server
-const echo = sockjs.createServer();
-
-//let online = []
-// on new connection event
-echo.on('connection', (conn) => {
-
-    online.push(conn.id)
-
-    // add this client to clients object
-    clients[conn.id] = conn;
-    //conn.write(online)
-    const newMessage = (mes) => {
-        console.log(mes);
-        broadcast({message:JSON.parse(mes)})
-}
-
-    const Disconnect = ()=> {
-        // let index = online.indexOf(conn.id);
-        // if (index > -1) {
-        //     online.splice(index, 1);
-        // }
-        // console.log(`disconnected`,conn.id)
-        delete clients[conn.id];
-    }
-
-    // on receive new data from client event
-    conn.on('data', newMessage)
-
-    // on connection close event
-    conn.on('close',Disconnect)
-
+/**
+ * perticuler channel defined
+ */
+let friends = server.registerChannel('friends');
+friends.on('connection', (conn) =>  {
+    conn.write('friends is connected');
+    conn.on('data', (data) => {
+        conn.write(data);
+    });
 });
 
-// Create an http server
-const server = http.createServer(app);
-
-// Integrate SockJS and listen on /echo
-echo.installHandlers(server, {prefix:'/echo'});
-
-// Start server
-server.listen(9999, '0.0.0.0');
-
-// Route for chat
-app.get('/chat', (req, res)=> {
-    res.sendFile('chat.html', {root: './public'});
+let Family = server.registerChannel('Family');
+Family.on('connection', (conn) => {
+    conn.write('Family is connected');
+    conn.on('data', (data) => {
+        conn.write(data);
+    });
 });
+
+let colleague = server.registerChannel('colleague');
+colleague.on('connection', (conn) => {
+    conn.write('coulegue is connected');
+    conn.on('data', (data) => {
+        conn.write(data);
+    });
+});
+
+let red = server.registerChannel('red');
+red.on('connection', (conn) => {
+    conn.write('Red is connected');
+    conn.on('data', function(data) {
+        conn.write(data);
+    });
+});
+
+let latency = server.registerChannel('latency');
+latency.on('connection', (conn) => {
+    conn.on('data', (data) => {
+        conn.write(data);
+    });
+});
+
+/**
+ * close channel
+ */
+red.on('close',() => {
+    console.log("SERVER: red channel was closed!");
+});
+
+/**
+ * channel end
+ */
+red.on('end', () => {
+    console.log("SERVER: red channel was end!");
+});
+
+let app = express();
+let serv = http.createServer(app);
+
+service.installHandlers(serv, {
+    prefix: '/multiplex'
+});
+
+console.log('[*] Listening on 0.0.0.0:9999');
+serv.listen(9999, '0.0.0.0');
+
+// Route for group chat
+app.get('/group', (req, res)=> {
+    res.sendFile('group.html', {root: './public'});
+});
+
